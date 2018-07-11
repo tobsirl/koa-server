af58d8a2aa8aa78c115b74a24ae7370c9db4bf41
Paul Tobin <tobster111@gmail.com>
2018-07-11 14:08:29 +0100

    Api Endpoints tested

--- routes/items.js
+++ routes/items.js
@@ -4,5 +4,8 @@ import ItemControllers from '../controllers/items';
 const router = new Router();
 
 router.get('/', ItemControllers.findAll);
+router.post('/items', ItemControllers.create);
+router.put('/items/:id', ItemControllers.update);
+router.delete('items/:id', ItemControllers.destroy);
 
 export default router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from 'koa-bodyparser';
 import helmet from 'koa-helmet';
 import logger from 'koa-logger';
 import routing from './routes/items';
+import router from './routes/items';
 
 // Connect to database
 mongoose
@@ -17,12 +18,15 @@ const app = new Koa();
 app
   .use(logger())
   .use(bodyParser())
-  .use(helmet());
+  .use(helmet())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 //routing(app);
+//app.use(router());
 
 app.listen(3000, () =>
-  console.log(`✅  The server is running at http://localhost:3000/`)
+  console.log(`The server is running at http://localhost:3000/`)
 );
 
 export default app;
